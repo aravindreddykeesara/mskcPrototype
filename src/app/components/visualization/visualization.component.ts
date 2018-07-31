66a0ea75c6064e859fb10430736d3489285e1593
Aravind Reddy Keesara <31300215+aravindreddykeesara@users.noreply.github.com>
2018-07-31 02:46:25 -0400

    added plotlyjs visuailzation, angular table

--- src/app/components/visualization/visualization.component.ts
+++ src/app/components/visualization/visualization.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../models/User';
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
-import { DataSource } from '@angular/cdk/collections';
 import { PatientdataService } from '../../services/patientdata.service';
 import { QueryModel } from '../../models/queryModel';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+
 
 
 
@@ -17,17 +17,30 @@ import { QueryModel } from '../../models/queryModel';
 })
 export class VisualizationComponent implements OnInit {
 
+  dataSource;
+  displayedColumns = ['name', 'age', 'disease', 'gender'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
   constructor(private patientService: PatientdataService) { }
-  ageCompareValue = 'greater';
+
+
+  ageCompareValue = null;
 
   diseases = ['brain tumor', 'bronchial tumor', 'cardiac tumor', 'gastric cancer', 'intraocular melanoma', 'kidney cancer', 'leukemia', 'liver cancer', 'lung cancer', 'neuralblastoma', 'pancreatic cancer', 'stomach cancer'];
   genders = ['male', 'female'];
   agecompare = ['greater', 'lesser'];
-
   model = new QueryModel(null, null, null);
+  graph = { data:null, layout: {} };
+  diseasevalue: string = '';
+  
 
   ngOnInit() {
+
+
   }
+
 
   clean(obj) {
     var propNames = Object.getOwnPropertyNames(obj);
@@ -39,27 +52,85 @@ export class VisualizationComponent implements OnInit {
     }
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   onSubmit() {
 
     this.clean(this.model);
+    this.diseasevalue = this.model.disease;
+   
 
     if (this.model.hasOwnProperty('age') && this.ageCompareValue != null) {
       if (this.ageCompareValue == ('greater')) {
         let temp = this.model.age;
         this.model.age = { "$gt": temp }
       }
-      else if (this.ageCompareValue == ('lesser')){
+      else if (this.ageCompareValue == ('lesser')) {
         let temp = this.model.age;
-      this.model.age = { "$lt": temp }
+        this.model.age = { "$lt": temp }
       }
+
+
 
     }
 
+
+
     this.patientService.queryToMongodb(this.model).subscribe(data => {
-      console.log("query log" + JSON.stringify(data));
+      this.plotlyMethod(data);
+      this.dataSource = new MatTableDataSource<User>(data);
+      this.dataSource.paginator = this.paginator;
     })
 
     this.model = new QueryModel(null, null, null);
+  }
+
+
+
+  plotlyMethod(data) {
+
+    let y = [];
+    //plotly
+    this.graph = {
+      data: [{
+        type: 'scatter',
+        x: data.map(a => a['age']),
+        y: Array.from({ length: data.length }, (v, k) => k = 1),
+        text: ['no.of patients'],
+        mode: 'markers',
+        marker: {
+          size: 10
+        },
+        transforms: [{
+          type: 'groupby',
+          groups: data.map(a => a['gender']),
+          styles: [
+            { target: 'male', value: { marker: { color: 'red' } } },
+            { target: 'female', value: { marker: { color: 'blue' } } }
+          ]
+        },
+        {
+          type: 'aggregate',
+          groups: data.map(a => a['age']),
+          aggregations: [
+            { target: 'y', func: 'sum', enabled: true },
+          ]
+        }
+
+        ]
+      }],
+      layout: {
+
+        title: 'Graph for selected disease : '+this.diseasevalue,
+        xaxis: { title: 'Age of patients', range: [0, 100]},
+        yaxis: { title: 'no.of patients'},
+
+
+      }
+    };
+   
 
 
   }
@@ -68,5 +139,3 @@ export class VisualizationComponent implements OnInit {
 
 
 }
-
-
