21535b1c861844e07671b98002ff723d839598a9
Aravind Reddy Keesara <31300215+aravindreddykeesara@users.noreply.github.com>
2018-07-31 02:49:49 -0400

    added responsive navbar,routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,10 +8,23 @@ import { AddinfoComponent } from './components/addinfo/addinfo.component';
 
 import {PatientdataService} from './services/patientdata.service';
 
-import {MatTableModule} from '@angular/material';
+import {
+  MatTableModule,
+  MatPaginatorModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatTabsModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import {HttpClientModule} from '@angular/common/http';
+import { PlotlyModule } from 'angular-plotly.js';
+
 import { HttpModule } from '@angular/http';
 import { VisualizationComponent } from './components/visualization/visualization.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 
@@ -19,17 +32,29 @@ import { VisualizationComponent } from './components/visualization/visualization
   declarations: [
     AppComponent,
     AddinfoComponent,
-    VisualizationComponent
+    VisualizationComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    HttpModule
+    HttpModule,
+    PlotlyModule,
+    MatPaginatorModule
   ],
   providers: [PatientdataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
