3ef2decb1883e00142dae26db50cbb7720364326
Aravind Reddy Keesara <31300215+aravindreddykeesara@users.noreply.github.com>
2018-07-31 02:50:39 -0400

    fixed import modules error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddinfoComponent } from './components/addinfo/addinfo.component';
 import { VisualizationComponent } from './components/visualization/visualization.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   { path: '', component: AddinfoComponent },
-  { path: 'visualize', component: VisualizationComponent }
+  { path: 'visualize', component: VisualizationComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
